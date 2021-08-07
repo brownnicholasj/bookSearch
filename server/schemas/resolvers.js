228aec4fd80266c57191ab6a225bb0b97fa3c00d
brownnicholasj <brown.nicholasj@gmail.com>
2021-08-06 20:32:53 -0500

    backend progress with server.js and schema creation (WIP)

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,84 @@
+const { Book, User } = require('../models');
+
+const resolvers = {
+	Query: {
+		me: async () => {
+			return User.find({});
+		},
+	},
+	Mutation: {
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				throw new AuthenticationError('Incorrect credentials');
+			}
+
+			const correctPw = await user.isCorrectPassword(password);
+
+			if (!correctPw) {
+				throw new AuthenticationError('Incorrect credentials');
+			}
+
+			const token = signToken(user);
+
+			return { token, user };
+		},
+
+		addUser: async (parent, { username, email, password }) => {
+			const user = await User.create({ username, email, password });
+
+			const token = signToken(user);
+
+			return { token, user };
+		},
+
+		saveBook: async (
+			parent,
+			[authors],
+			description,
+			bookId,
+			image,
+			link,
+			title,
+			context
+		) => {
+			if (context.user) {
+				const book = await Book.create({
+					authors,
+					description,
+					bookId,
+					image,
+					link,
+					title,
+				});
+
+				await User.findOneAndUpdate(
+					{ _id: context.user._id },
+					{ $addToSet: { savedBooks: book._id } }
+				);
+
+				return book;
+			}
+			throw new AuthenticationError('You need to be logged in!');
+		},
+
+		removeBook: async (parent, bookId, context) => {
+			if (context.user) {
+				const book = await Book.findOneAndDelete({
+					_id: bookId,
+				});
+
+				await User.findOneAndUpdate(
+					{ _id: context.user._id },
+					{ $pull: { savedBooks: book._id } }
+				);
+
+				return book;
+			}
+			throw new AuthenticationError('You need to be logged in!');
+		},
+	},
+};
+
+module.exports = resolvers;
